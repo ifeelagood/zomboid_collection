5060cf464eff1f689e83f97a2a8c5e9e383d7603
ifeelagood <thicccatto3@gmail.com>
2023-12-10 19:05:36 +1100

    refactor: cleaned up main, seperated functionality, etc.

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 The game "Project Zomboid" utilises the steam workshop for mods. However, when hosting a dedicated server, mods are installed via modifying the server's "SERVERNAME.ini" file delimeted by semicolons:
 
 `Mods=` - List of mod IDs to be enabled on the server.
+
 `WorkshopItems=` - List of workshop IDs to be downloaded on the server.
+
 `Map=` - Maps to be loaded, with priority in order of appearance.
 
 Given a steam workshop collection, this script will:
@@ -12,15 +14,30 @@ Given a steam workshop collection, this script will:
 3. Prompt the user to select which maps to load, if multiple maps are available
 4. Generate a "SERVERNAME.ini" file with the selected mods and maps.
 
+## Quick Start
+
+`python3 get_collection.py -c COLLECTION_ID -o SERVERNAME.ini`
+
 ## Usage
 
 `--collection, -c` - Steam workshop collection ID. This can be found in the URL of the collection. For example, the collection ID for the collection `https://steamcommunity.com/sharedfiles/filedetails/?id=123456789` is `123456789`.
 
-`--servername, -n` - Name of the server. This will be used to name the generated "SERVERNAME.ini" file.
+`--output, -o` - Path to output ini file. Defaults to `servertest.ini` in the current directory.
+
+`--overwrite, -w` - Overwrite fields in output ini file if it already exists. Defaults to `False`.
 
 `--threads, -t` - Number of threads to use when downloading workshop items. Defaults to 16.
 
+`--no-cache` - Do not use cached requests. This will force the script to make requests for mod IDs and workshop items. Use if collection/mods has been updated recently.
+
+`--no-dependencies` - Do not add dependencies for workshop items. Defaults to `False`.
+
 `--help, -h` - Display help message.
+
+## Installation
+
+1. Install Python. This script was developed using Python 3.11.9, but should work with any version of Python 3.
+2. Install the required packages using `pip install -r requirements.txt`
 
 ## Features
 
@@ -31,3 +48,9 @@ Problems I encountered when using other available scripts:
 - Some do not handle maps at all. This one makes sure "Mauldraugh, KY" is always last in the list.
 
 - Some are very slow. This one uses multithreading to download workshop items.
+
+Additional Features:
+
+- Request caching. As selecting mod ids can be a finicky process (especially when there are a lot of mods), the script will cache the requests for mod IDs and workshop items. This means that if you run the script again with the same collection ID, it will not need to make the requests again.
+
+- Dependency Resolution. If a workshop item has dependencies, the script will automatically and recursively add them to the list.
--- get_collection.py
+++ get_collection.py
@@ -1,8 +1,9 @@
 import argparse
 import dataclasses
 import datetime
-import re
+import logging
 import typing
+import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import requests
@@ -12,89 +13,23 @@ from bs4 import BeautifulSoup
 
 from pick import pick
 
-MOD_ID_REGEX = "(?:Mod(?:\s?)ID)(?:\:)(?:[\s+])([\w._&-]+)(?:\r?\n)?"
-MAP_FOLDER_REGEX = "(?:Map(?:\s?)Folder)(?:\:)(?:\s+)([^\r\n]+)"
+from fetchers import Fetcher
+from parsers import CollectionParser, ItemParser
+from ini import zINI
 
 MAP_BASE = "Muldraugh, KY"
-
 WORKSHOP_FILE_URL = "https://steamcommunity.com/sharedfiles/filedetails/?id={}"
 
-
-def yield_workshop_ids(session : requests.Session | requests_cache.CachedSession, collection_id : int) -> int:
-    try:
-        url = WORKSHOP_FILE_URL.format(collection_id)
-        response = session.get(url)
-
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print(f"Error: {e}")
-        return
-    
-
-    soup = BeautifulSoup(response.text, "html.parser")
-    for div in soup.find_all("div", class_="collectionItem"):
-        item_id = int(div["id"].strip("sharedfiles_"))
-        yield item_id
-
-def get_workshop_soup(session : requests.Session | requests_cache.CachedSession, item_id : int) -> BeautifulSoup:
-    try:
-        url = WORKSHOP_FILE_URL.format(item_id)
-        response = session.get(url)
-
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print(f"Error: {e}")
-        return
-    
-    return BeautifulSoup(response.text, "html.parser")
-
-
-def parse_workshop_description(soup : BeautifulSoup) -> str:
-    div = soup.find("div", class_="workshopItemDescription")
-    
-    # hack: add line breaks <br>
-    for br in div.find_all("br"):
-        br.replace_with("\n")
-    
-    return div.text
-
-def parse_dependencies(soup : BeautifulSoup) -> typing.Set[int]:
-    dependencies = set()
-    
-    div = soup.find("div", class_="requiredItemsContainer")
-    
-    if div is not None:
-        for a in div.find_all("a"):
-            # get after id=
-            dependencies.add(int(a["href"].split("=")[1])) # TODO hacky
-            
-    return dependencies
-
-def parse_mod_ids(description : str) -> typing.Set[str]:
-    matches = re.findall(MOD_ID_REGEX, description)
-    mod_ids = set()
-    
-    for m in matches:
-        mod_ids.add(m.strip())
-            
-    return mod_ids
-
-def parse_map_folders(description : str) -> typing.Set[str]:
-    matches = re.findall(MAP_FOLDER_REGEX, description)
-    map_folders = set()
-    
-    for m in matches:
-        map_folders.add(m.strip())
-    
-
-    return map_folders
+DELIMITER = ";"
         
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Generate server configuration for a Project Zomboid workshop collection")
     
     parser.add_argument("--collection-id", "-c", type=int, help="Workshop collection ID", required=True)
-    parser.add_argument("--server-name", "-n", help="Server name", required=True)
+    parser.add_argument("--output", "-o", help="Output zomboid ini file", default="servertest.ini")
+    parser.add_argument("--overwrite", action="store_true", help="Overwrite output fields if they already exist")
     parser.add_argument("--threads", "-t", type=int, help="Number of threads to use", default=16)
     parser.add_argument("--no-cache", action="store_true", help="Disable requests cache. Useful if collection was updated recently")
     parser.add_argument("--no-dependencies", action="store_true", help="Do not add unresolved mod dependencies")
@@ -112,35 +47,42 @@ class WorkshopItem:
     def __hash__(self):
         return hash(self.workshop_id)
 
-
-def scrape_workshop_items(session : requests.Session | requests_cache.CachedSession, args : argparse.Namespace, workshop_ids : typing.Set[int]) -> typing.Set[WorkshopItem]:
-    workshop_items = set()
+    def __str__(self):
+        return WORKSHOP_FILE_URL.format(self.workshop_id)
     
+    def __repr__(self):
+        return str(self)
+
+def scrape_workshop_items(fetcher : Fetcher, threads : int, workshop_ids : typing.Set[int]) -> typing.Set[WorkshopItem]:
     def process_item(item_id):
-        soup = get_workshop_soup(session, item_id)
+        soup = fetcher.fetch_soup(item_id)
         
-        workshop_items.add(WorkshopItem(
+        desc = ItemParser.parse_description(soup)
+        
+        item = WorkshopItem(
             workshop_id=item_id,
-            dependencies=parse_dependencies(soup),
-            mod_ids=parse_mod_ids(parse_workshop_description(soup)),
-            map_folders=parse_map_folders(parse_workshop_description(soup))
-        ))    
+            dependencies=ItemParser.parse_dependencies(soup),
+            mod_ids=ItemParser.parse_mod_ids(desc),
+            map_folders=ItemParser.parse_map_folders(desc)
+        )
         
+        return item
         
-    with ThreadPoolExecutor(max_workers=args.threads) as executor:
+    workshop_items = set()
+    with ThreadPoolExecutor(max_workers=threads) as executor:
         futures = []
         for item_id in workshop_ids:
             futures.append(executor.submit(process_item, item_id))
             
         for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
-            future.result()
+            workshop_items.add(future.result())
             
     return workshop_items
 
 def select_mods_maps(workshop_items : typing.Set[WorkshopItem]):
     for item in workshop_items:
         if len(item.mod_ids) == 0:
-            print(f"Warning: Workshop item {item.workshop_id} has no mod IDs")
+            print(f"Warning: Workshop item {item} has no mod IDs")
         
         if len(item.mod_ids) > 1:
             selected_mod_ids = pick(list(item.mod_ids), f"Select mods for workshop item {WORKSHOP_FILE_URL.format(item.workshop_id)}", multiselect=True, min_selection_count=1)
@@ -150,57 +92,80 @@ def select_mods_maps(workshop_items : typing.Set[WorkshopItem]):
             selected_map_folders = pick(list(item.map_folders), f"Select map folders for workshop item {WORKSHOP_FILE_URL.format(item.workshop_id)}", multiselect=True, min_selection_count=1)
             item.map_folders = set(t[0] for t in selected_map_folders)
 
+def gather_unresolved_dependencies(workshop_items : typing.Set[WorkshopItem]):
+    workshop_ids = set(item.workshop_id for item in workshop_items)
+    
+    # gather dependencies
+    dependencies = set()
+    for item in workshop_items:
+        for dependency in item.dependencies:
+            dependencies.add(dependency)
+            
+            if dependency not in workshop_ids:
+                logging.warning(f"Found unresolved dependency {dependency} for workshop item {item}")
+
+    unresolved = dependencies - workshop_ids
+    return unresolved
+
+def resolve_dependencies(fetcher : Fetcher, threads : int, workshop_items : typing.Set[WorkshopItem]):
+    workshop_ids = set(item.workshop_id for item in workshop_items)
+    
+    unresolved = gather_unresolved_dependencies(workshop_items)    
+
+    # resolve dependencies
+    dependencies = set()
+    while len(unresolved) > 0:
+        logging.info(f"Resolving {len(unresolved)} unresolved dependencies")
+        for item in scrape_workshop_items(fetcher, threads, unresolved):
+            workshop_items.add(item)
+            workshop_ids.add(item.workshop_id)
+            
+            for dependency in item.dependencies:
+                dependencies.add(dependency)
+                
+                if dependency not in workshop_ids:
+                    logging.warning(f"Found unresolved dependency {dependency} for workshop item {item}")
+                    
+        unresolved = dependencies - workshop_ids
+
+def write_config(ini_path : os.PathLike, workshop_items : typing.Set[WorkshopItem], overwrite : bool):
+    if os.path.exists(ini_path):
+        if not overwrite:
+            logging.error(f"File {ini_path} already exists. Use --overwrite to overwrite")
+            return
+
+        ini = zINI.load(ini_path)
+    else:
+        ini = zINI()
+    
+    workshop_ids = set(str(item.workshop_id) for item in workshop_items)
+    mod_ids = set(mod_id for item in workshop_items for mod_id in item.mod_ids)
+    map_folders = set(map_folder for item in workshop_items for map_folder in item.map_folders)
+    
+    ini["WorkshopItems"] = DELIMITER.join(workshop_ids)
+    ini["Mods"] = DELIMITER.join(mod_ids)
+    ini["Map"] = DELIMITER.join(map_folders) + DELIMITER + MAP_BASE
+    
+    ini.save(ini_path)
+
 def main():
     args = get_args()
 
     # create requests cache session
     session = requests.Session() if args.no_cache else requests_cache.CachedSession(".requests_cache", expire_after=datetime.timedelta(minutes=30))
+    fetcher = Fetcher(session)
 
     # scrape workshop items
-    workshop_items = scrape_workshop_items(session, args, set(yield_workshop_ids(session, args.collection_id)))
-    select_mods_maps(workshop_items) # this modifies workshop_items
+    workshop_ids = set(CollectionParser.yield_workshop_ids(fetcher.fetch_soup(args.collection_id)))
+    workshop_items = scrape_workshop_items(fetcher, args.threads, workshop_ids)
+    select_mods_maps(workshop_items)
     
-    # extract mod IDs and map folders
-    # prompt user to select mods and map folders
-    workshop_ids = set()
-    dependencies = set()
-    mod_ids = set()
-    map_folders = set()
-    
-    for item in workshop_items:
-        workshop_ids.add(item.workshop_id)
-        dependencies.update(item.dependencies)
-        mod_ids.update(item.mod_ids)
-        map_folders.update(item.map_folders)
-    
-    missing_dependencies = dependencies - workshop_ids
-    if len(missing_dependencies) > 0:
-        print(f"Warning: {len(missing_dependencies)} dependencies are missing from the collection")
+    # resolve dependencies
+    if not args.no_dependencies:
+        resolve_dependencies(fetcher, args.threads, workshop_items)
         
-        for item_id in missing_dependencies:
-            print(f"Mising: {WORKSHOP_FILE_URL.format(item_id)}")
-            
-        if not args.no_dependencies:
-            dependency_items = scrape_workshop_items(args, missing_dependencies)
-            select_mods_maps(dependency_items)
-            
-            for item in dependency_items:
-                workshop_ids.add(item.workshop_id)
-                dependencies.update(item.dependencies)
-                mod_ids.update(item.mod_ids)
-                map_folders.update(item.map_folders)
-
-
-
-    # write result to ini file
-    ini_path = f"{args.server_name}.ini"
     
-    with open(ini_path, "w") as f:
-        f.write("WorkshopItems=" + ";".join(map(str, workshop_ids)) + "\n")
-        f.write("Mods=" + ";".join(mod_ids) + "\n")
-        f.write("Map=" + ";".join(map_folders) + ";" + MAP_BASE + "\n")
-    
-    print("Done")
+    write_config(args.output, workshop_items, args.overwrite)
         
 if __name__ == "__main__":
     main()
